2cce3ae7ca20e868c5117372a41ec8628fc8083e
dcvslab <dcvslab@users.noreply.github.com>
2015-09-29 16:55:36 -0400

    Create dubniumbeta.js

---
+++ dubniumbeta.js
@@ -0,0 +1,9 @@
+//Dubnium Script
+//A script for dubtrack.fm
+//Created by DCV & MatheusAvellar
+//This code may or may not work, as it is an unofficial, beta test for the actual release!
+
+var dubnium = {
+  version: "Alpha 01",
+}
+  
