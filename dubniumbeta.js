ada0ff3665ec7dcd141ff99b6ca7614202e1a95d
dcvslab <dcvslab@users.noreply.github.com>
2015-09-29 17:08:40 -0400

    A1.0.0B

--- dubniumbeta.js
+++ dubniumbeta.js
@@ -4,6 +4,9 @@
 //This code may or may not work, as it is an unofficial, beta test for the actual release!
 
 var dubnium = {
-  version: "Alpha 01",
+  version: "A1.0.0B",
+  chat: document.getElementsByClassName("chat-main")[0],
+  addchat: function addChat(username, message) { $(dubnium.chat).append("<li class='user-dubniumscript'><div class='stream-item-content'><div class='image_row'></div><div class='activity-row'><div class='text'><p><a class='username'>" + username + "</a> " + message + "</p></div><div class='meta-info'><span class='username'>Dubnium </span><i class='icon-dot'></i><span class='timeinfo'><time title='Dubnium' class='timeago' datetime='Dubnium'>Dubnium Script</time></span></div></div></div></li>")}
 }
+dubnium.addchat("Dubnium v" + dubnium.version + "", "has started successfully!")
   
