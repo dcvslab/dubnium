1f43da60a0dc2bcc2f94516ec4148c1003dfd8d2
dcvslab <dcvslab@users.noreply.github.com>
2015-10-01 15:57:00 -0400

    Update dubnium.addchat

--- dubniumbeta.js
+++ dubniumbeta.js
@@ -11,7 +11,7 @@
  *
  */
 
-const DBVERSION = "A1.0.3B";
+const DBVERSION = "A1.0.4B";
 
 var _head = document.getElementsByTagName("head")[0];
 $(_head).append("<link rel='stylesheet' type='text/css' href='https://rawgit.com/dcvslab/dubnium/master/dubnium.css'>");
@@ -22,30 +22,15 @@ var dubnium = {
         chat: document.getElementsByClassName("chat-main")[0],
         dubup: document.getElementsByClassName("dubup")[0]
     },
-    addchat: function addChat(_username, _message) {
-        _username = (!_username) ? "Dubnium" : _username;
-        _message = (!_message) ? "Message" : _message;
+    addchat: function addChat(_message) {
+        _message = (!_message) ? "Dubnium" : _message;
 
         var _c = $(".chat-messages")[0];
         var _shouldScroll = _c.scrollTop > _c.scrollHeight - $(".chat-messages").height() - 50;
-
+        
         $(dubnium.site.chat).append(
-            "<li class='user-dubniumscript'>"
-            +   "<div class='stream-item-content'>"
-            +       "<div class='image_row'></div>"
-            +       "<div class='activity-row'>"
-            +           "<div class='text'>"
-            +               "<p><a class='username'>" + _username + "</a> " + _message + "</p>"
-            +           "</div>"
-            +           "<div class='meta-info'>"
-            +               "<span class='username'>Dubnium </span>"
-            +               "<i class='icon-dot'></i>"
-            +               "<span class='timeinfo'>"
-            +                   "<time title='Dubnium' class='timeago' datetime='Dubnium'>Dubnium Script</time>"
-            +               "</span>"
-            +           "</div>"
-            +       "</div>"
-            +   "</div>"
+            "<li class='chat-system-loading'>"
+                +"" + _message
             +"</li>");
 
         if (_shouldScroll)  _c.scrollTop = _c.scrollHeight;
@@ -54,4 +39,4 @@ var dubnium = {
 }
 //TEMP AUTO DUBUP 
 Dubtrack.Events.bind('realtime:room_playlist-update', function(){ dubnium.site.dubup.click() });
-dubnium.addchat("Dubnium v" + dubnium.version + "", "has started successfully!");
+dubnium.addchat("Dubnium v" + dubnium.version + "has started successfully!");
