129d748abbcd38024ff0470f926bdd35512b682c
dcvslab <dcvslab@users.noreply.github.com>
2015-10-01 21:29:48 -0400

    more-menu-things.js

--- dubniumbeta.js
+++ dubniumbeta.js
@@ -11,7 +11,7 @@
  *
  */
 
-const DBVERSION = "A1.0.6B";
+const DBVERSION = "A1.0.7B";
 
 var _head = document.getElementsByTagName("head")[0];
 $(_head).append("<link rel='stylesheet' type='text/css' href='https://rawgit.com/dcvslab/dubnium/master/dubnium.css'>");
@@ -22,10 +22,11 @@ var dubnium = {
         menuright: document.getElementById("main-menu-right"),
         navbar: document.getElementsByClassName("user-header-menu")[0],
         pmbutton: document.getElementsByClassName("user-messages-button")[0],
+        pm: document.getElementById("private-messages"),
         chat: document.getElementsByClassName("chat-main")[0],
         loading:{
             loading: document.getElementById("main-loading"),
-            text: document.getElementsByClassName("loading-text"),
+            text: document.getElementsByClassName("loading-text")[0],
             spinner: document.getElementsByClassName("spinner")[0],
         },
         dubup: document.getElementsByClassName("dubup")[0],
@@ -37,17 +38,22 @@ var dubnium = {
             buttontext: document.createElement("span"),
             createbutton: function createButton() {
                 dubnium.menu.button.button.id = "dbbtn";
+                dubnium.menu.button.button.className = "user-messages-button";
                 dubnium.menu.button.buttontext.innerHTML = "dubnium";
                 dubnium.menu.button.buttonbtn.appendChild(dubnium.menu.button.buttontext); 
                 dubnium.menu.button.button.appendChild(dubnium.menu.button.buttonbtn);
                 dubnium.site.navbar.insertBefore(dubnium.menu.button.button, dubnium.site.navbar.childNodes[0]);
+                dubnium.menu.button.button.addEventListener("click", function(){ dubnium.menu.menu.menu.style.display = "block"; dubnium.site.pm.style.display = "none"; }
+                dubnium.site.pmbutton.addEventListener("click", function(){ dubnium.menu.menu.menu.style.display = "none"; dubnium.site.pm.style.display = "block"; }
             },
         },
         menu:{
             menu: document.createElement("section"),
             createmenu: function createMenu() {
-                dubnium.menu.menu.menu.id = "dbmenu"
-            }
+                dubnium.menu.menu.menu.id = "dbmenu";
+                dubnium.site.menuright.appendChild(dubnium.menu.menu.menu);
+                
+            },
             
         }
     },
@@ -71,3 +77,4 @@ Dubtrack.Events.bind('realtime:room_playlist-update', function(){ dubnium.site.d
 
 dubnium.menu.button.createbutton()
 dubnium.addchat("<span style='color:#f0f'>Dubnium v" + dubnium.version + " has started successfully!</span>");
+
