754878701e74d28afb509bf517377b389be00621
dcvslab <dcvslab@users.noreply.github.com>
2015-10-01 23:20:05 -0400

    menu + option
    it isn't toggleable, but whatever.

--- dubniumbeta.js
+++ dubniumbeta.js
@@ -11,17 +11,15 @@
  *
  */
 
-const DBVERSION = "A1.0.7B";
-
 var _head = document.getElementsByTagName("head")[0];
 $(_head).append("<link rel='stylesheet' type='text/css' href='https://rawgit.com/dcvslab/dubnium/master/dubnium.css'>");
 
 var dubnium = {
-    version: DBVERSION,
+    version: "A1.1.0B",
     site:{ 
         menuright: document.getElementById("main-menu-right"),
         navbar: document.getElementsByClassName("user-header-menu")[0],
-        pmbutton: document.getElementsByClassName("user-messages-button")[0],
+        pmbutton: document.getElementsByClassName("user-messages")[0],
         pm: document.getElementById("private-messages"),
         chat: document.getElementsByClassName("chat-main")[0],
         loading:{
@@ -31,6 +29,31 @@ var dubnium = {
         },
         dubup: document.getElementsByClassName("dubup")[0],
     },
+    settings:{
+      autoupdub: "true"  
+    },
+    functions:{
+        addchat: function addChat(_class, _message) {
+        _class = (!_class) ? "system": _class;
+        _message = (!_message) ? "Dubnium" : _message;
+
+        var _c = $(".chat-messages")[0];
+        var _shouldScroll = _c.scrollTop > _c.scrollHeight - $(".chat-messages").height() - 50;
+        
+        $(dubnium.site.chat).append(
+            "<li class='" + _class + "'>" //chat-system-loading or system
+                +"" + _message
+            +"</li>");
+
+        if (_shouldScroll)  _c.scrollTop = _c.scrollHeight;
+        if ($(".chat-messages").children().length > 512) $(".chat-messages").children().first().remove();
+        },
+        autoupdub: function autoUpdub() {
+            if (dubnium.settings.autoupdub == "true") {
+                dubnium.site.dubup.click(); 
+            };
+        },
+    },
     menu:{
         button:{
             button: document.createElement("li"),
@@ -38,43 +61,33 @@ var dubnium = {
             buttontext: document.createElement("span"),
             createbutton: function createButton() {
                 dubnium.menu.button.button.id = "dbbtn";
-                dubnium.menu.button.button.className = "user-messages-button";
+                dubnium.menu.button.button.className = "user-messages";
                 dubnium.menu.button.buttontext.innerHTML = "dubnium";
                 dubnium.menu.button.buttonbtn.appendChild(dubnium.menu.button.buttontext); 
                 dubnium.menu.button.button.appendChild(dubnium.menu.button.buttonbtn);
                 dubnium.site.navbar.insertBefore(dubnium.menu.button.button, dubnium.site.navbar.childNodes[0]);
-                dubnium.menu.button.button.addEventListener("click", function(){ dubnium.menu.menu.menu.style.display = "block"; dubnium.site.pm.style.display = "none"; })
-                dubnium.site.pmbutton.addEventListener("click", function(){ dubnium.menu.menu.menu.style.display = "none"; dubnium.site.pm.style.display = "block"; })
+                dubnium.menu.button.button.addEventListener("click", function(){ dubnium.site.pm.style.display = "none"; })
+                dubnium.site.pmbutton.addEventListener("click", function(){ dubnium.site.pm.style.display = "block"; })
             },
         },
         menu:{
             menu: document.createElement("section"),
+            menuc: document.createElement("div"), //menu content
             createmenu: function createMenu() {
                 dubnium.menu.menu.menu.id = "dbmenu";
+                dubnium.menu.menu.menuc.className = "main-message-list";
+                $(dubnium.menu.menu.menuc).append("<li class='message-item display-0-users dbms'><div class='message-content'><h3 class='dbmsh'>Auto Updub</h3><p class='dbmsb'>ON</p></div></li>");
+                dubnium.menu.menu.menu.appendChild(dubnium.menu.menu.menuc);
                 dubnium.site.menuright.appendChild(dubnium.menu.menu.menu);
                 
             },
             
-        }
+        },
     },
-    addchat: function addChat(_message) {
-        _message = (!_message) ? "Dubnium" : _message;
-
-        var _c = $(".chat-messages")[0];
-        var _shouldScroll = _c.scrollTop > _c.scrollHeight - $(".chat-messages").height() - 50;
-        
-        $(dubnium.site.chat).append(
-            "<li class='chat-system-loading'>"
-                +"" + _message
-            +"</li>");
-
-        if (_shouldScroll)  _c.scrollTop = _c.scrollHeight;
-        if ($(".chat-messages").children().length > 512) $(".chat-messages").children().first().remove();
-    }
 }
-//TEMP AUTO DUBUP 
-Dubtrack.Events.bind('realtime:room_playlist-update', function(){ dubnium.site.dubup.click() });
+Dubtrack.Events.bind('realtime:room_playlist-update', dubnium.functions.autoupdub());
 
 dubnium.menu.button.createbutton()
-dubnium.addchat("<span style='color:#f0f'>Dubnium v" + dubnium.version + " has started successfully!</span>");
+dubnium.menu.menu.createmenu()
+dubnium.functions.addchat("chat-system-loading", "<span style='color:#f0f'>Dubnium v " + dubnium.version + " has started successfully!</span>");
 
